ae1304c783eaaa9f9c363b1b5e53dffc41409a71
zcfrank1st <zcfrank1st@sina.com>
2016-12-28 15:31:36 +0800

    add stage logo

--- src/main/java/com/chaos/fx/hades/Main.java
+++ src/main/java/com/chaos/fx/hades/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -15,6 +16,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(ClassLoader.getSystemResource("first.fxml"));
 
         primaryStage.setTitle("Hades Fx");
+        primaryStage.getIcons().add(new Image(ClassLoader.getSystemResource("hades.png").toExternalForm()));
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.setResizable(false);
         primaryStage.show();
